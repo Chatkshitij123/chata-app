f3bd20fd0d928efcdae9d16c38825874c5d02797
Vaibhaw Mishra <vaibhavmishra658@gmail.com>
2024-01-26 13:51:47 +0530

    Fixed cors and userId errors

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
 const express = require("express"); // web framework for Node.js
 
-const routes = require("./routes/index")
+const routes = require("./routes/index");
 
-const morgan = require("morgan");//HTTP request logger middleware for node.js
+const morgan = require("morgan"); //HTTP request logger middleware for node.js
 
 const rateLimit = require("express-rate-limit");
 
-const helmet = require("helmet");//
+const helmet = require("helmet"); //
 
-const mongosanitize = require('express-mongo-sanitize');
+const mongosanitize = require("express-mongo-sanitize");
 
 const bodyParser = require("body-parser");
 
-const xss = require('xss');//cross-side-scripting //we can again send some scripts,malicious code in the req that is send to our
+const xss = require("xss"); //cross-side-scripting //we can again send some scripts,malicious code in the req that is send to our
 //server and to filter them out we use this package//sanitize the untrusted html
 
 const cors = require("cors");
@@ -20,38 +20,41 @@ const cors = require("cors");
 //to talk between both we use cors
 const app = express();
 
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: true,
-}));
+  })
+);
 
 app.use(mongosanitize());
 
-// 
-
+//
 
 //middleware
-app.use(cors({
-    origin: "http://localhost:3001",//from where the req is going to be originated//every domain-*
-    methods: ['GET', "PATCH", "POST", "DELETE", "PUT"],
-    credentials: true,//there is a header name- accesscontrolallowcredentials that is set to false by default and we are change
-    //it to true to tell the browsers to expose the response to our frontend javascript code 
-}));
-app.use(express.json({limit: "10kb"}));
-app.use(bodyParser.json());//it is just passing the json-data
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(
+  cors({
+    origin: "*", //from where the req is going to be originated//every domain-*
+    methods: ["GET", "PATCH", "POST", "DELETE", "PUT"],
+    credentials: true, //there is a header name- accesscontrolallowcredentials that is set to false by default and we are change
+    //it to true to tell the browsers to expose the response to our frontend javascript code
+  })
+);
+app.use(express.json({ limit: "10kb" }));
+app.use(bodyParser.json()); //it is just passing the json-data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(helmet());
 //this is going to set various headers in our response
-if(process.env.NODE_ENV === "development"){
-    app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 //dev is just basically an option that we are going to pass
 //we are in the development mode and we need as much as info for each and every request
 
 const limiter = rateLimit({
-    max: 3000,
-    windowMs: 60 * 60 * 1000,//in one hour
-    message: "Too many requests from this IP, please try again in an hour",
+  max: 3000,
+  windowMs: 60 * 60 * 1000, //in one hour
+  message: "Too many requests from this IP, please try again in an hour",
 });
 //max denotes how many req we received
 
@@ -62,4 +65,4 @@ app.use(routes);
 
 module.exports = app;
 
-//https://localhost:3000/v1/auth/login => 
+//https://localhost:3000/v1/auth/login =>
